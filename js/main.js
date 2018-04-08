69043c2daff5026e91570568ceba194085b45943
Raysa <aurrecochearaysa@gmail.com>
2018-04-08 17:02:12 -0500

    UI almost done

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const colors = ['#42bcf4', '#95f441', '#f47041', '#f141f4', '#f4f141', '#af41f4', '#4155f4' ];
+
+function getRandomColor(arr) {
+    let index = Math.floor(Math.random() * Math.floor(arr.length));
+    return arr[index]
+}
+
+function getQuote() {
+    $.ajax({
+        
+    });
+}
