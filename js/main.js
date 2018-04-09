b7426a737e15438c323a77e182d4f0276af6a5a6
Raysa <aurrecochearaysa@gmail.com>
2018-04-09 18:23:25 -0500

    social media buttons working properly

--- js/main.js
+++ js/main.js
@@ -63,4 +63,10 @@ function getQuote() {
 $(function() {
     console.log('page loaded');
     $('#new-quote').on('click', getQuote);
+    $('#tweet-quote').on('click', function() {
+        openUrl( 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '"' + currentAuthor));
+    });
+    $('#tumblr-quote').on('click', function() {
+        openUrl('https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption='+encodeURIComponent(currentAuthor) + '&content=' + encodeURIComponent(currentQuote) + '&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button');
+    });
 });
